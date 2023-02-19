769e47b2ca230b21c07b6b4ae30137ba5d1d71a3
San4o5 <babaimal2006@gmail.com>
2023-02-19 13:39:36 +0200

    s

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print("Hello Waorld")
+import pygame
+from game_object import *
+
+
+pygame.init()
+
+
+window = pygame.display.set_mode((800,500))
+pygame.display.set_caption("Ping-Pong")
+
+def run():
+    game = True
+    clock = pygame.time.Clock()
+    player1 = Board(50, 250, 20, 100, None, 5)
+
+
+
+
+    while game:
+        window.fill((0,0,0))
+        pygame.draw.rect(window, (255,0,0), player1)
+        player1.move()
+
+
+        for event in pygame.event.get():
+            if event.tupe == pygame.QUIT:
+                game = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player1.MOVE["UP"] = True
+                if event.key == pygame.K_s:
+                    player1.MOVE["DOWN"] = True
+            
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    player1.MOVE["UP"] = False
+                if event.key == pygame.K_s:
+                    player1.MOVE["DOWN"] = False
+
+
+        
+        clock.tick(60)
+        pygame.display.flip()
+
+run()
