26f470809a46c520051cead392046dcb59bc094b
San4o5 <babaimal2006@gmail.com>
2023-02-26 13:33:40 +0200

    Upgrade (two player)

--- game_object.py
+++ game_object.py
@@ -1,6 +1,6 @@
 import pygame
-
-
+from data import *
+from random import choice, uniform
 class Board(pygame.Rect):
     def __init__(self, x, y, width, height, image, speed):
         super().__init__(x, y, width, height)
@@ -12,8 +12,39 @@ class Board(pygame.Rect):
         if self.MOVE["UP"] and self.y > 0:
             self.y -= self.SPEED
         
-        elif self.MOVE["DOWN"] and self.y < 400:
+        elif self.MOVE["DOWN"] and self.y < set_win["HEIGHT"]- self.height:
             self.y += self.SPEED
+
+class Ball():
+    def __init__(self, x, y, radius, color, image, speed):
+        self.X = x
+        self.Y = y
+        self.RADIUS = radius
+        self.COLOR = color
+        self.IMAGE = image
+        self.SPEED = speed
+        self.ANGLE = choice([-self.SPEED, self.SPEED])
+        self.VERTICAL = 0
+        self.DIRECTION = True if self.ANGLE < 0 else False
+
+    def move(self, board):
+        
+        if self.Y <= 0 or self.Y + self.RADIUS >= set_win["HEIGHT"]:
+            self.VERTICAL *= -1
+        elif board.collidepoint(self.X - self.RADIUS, self.Y) or board.collidepoint(self.X + self.RADIUS, self.Y):
+            if board.MOVE["UP"]:
+                self.VERTICAL = - uniform(self.SPEED // 2, self.SPEED)
+            if board.MOVE["DOWN"]:
+                self.VERTICAL = uniform(self.SPEED // 2, self.SPEED)
+            self.ANGLE *= -1
+            self.DIRECTION = not self.DIRECTION
+        
+        self.X += self.ANGLE
+        self.Y += self.VERTICAL
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,42 +1,59 @@
 import pygame
 from game_object import *
-
-
+from data import *
+from random import choice
 pygame.init()
 
 
-window = pygame.display.set_mode((800,500))
+window = pygame.display.set_mode((set_win["WIDTH"], set_win["HEIGHT"]))
 pygame.display.set_caption("Ping-Pong")
 
 def run():
     game = True
     clock = pygame.time.Clock()
-    player1 = Board(50, 250, 20, 100, None, 5)
-
-
+    player_left = Board(50, 250, 20, 100, None, 5)
+    player_right = Board(750, 250, 20, 100, None, 5)
+    ball = Ball(set_win["WIDTH"] // 2, set_win["HEIGHT"] // 2, 20, (0,255,0), None, 5)
 
 
     while game:
         window.fill((0,0,0))
-        pygame.draw.rect(window, (255,0,0), player1)
-        player1.move()
+        pygame.draw.rect(window, (255,0,0), player_left)
+        pygame.draw.rect(window, (255,0,0), player_right)
+        pygame.draw.circle(window, ball.COLOR, (ball.X, ball.Y), ball.RADIUS)
+        player_left.move()
+        player_right.move()
+        #ball.move(player_left, player_right)
 
+        if ball.DIRECTION:
+            ball.move(player_left)
+        else:
+            ball.move(player_right)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    player1.MOVE["UP"] = True
+                    player_left.MOVE["UP"] = True
                 if event.key == pygame.K_s:
-                    player1.MOVE["DOWN"] = True
+                    player_left.MOVE["DOWN"] = True
+                ##
+                if event.key == pygame.K_UP:
+                    player_right.MOVE["UP"] = True
+                if event.key == pygame.K_DOWN:
+                    player_right.MOVE["DOWN"] = True
             
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_w:
-                    player1.MOVE["UP"] = False
+                    player_left.MOVE["UP"] = False
                 if event.key == pygame.K_s:
-                    player1.MOVE["DOWN"] = False
-
+                    player_left.MOVE["DOWN"] = False
+                ##
+                if event.key == pygame.K_UP:
+                    player_right.MOVE["UP"] = False
+                if event.key == pygame.K_DOWN:
+                    player_right.MOVE["DOWN"] = False
 
         
         clock.tick(60)
